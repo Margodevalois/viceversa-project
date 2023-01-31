43a1eed41787e858e72047a57729a0418db0b315
Margarita <margodevalois@gmail.com>
2023-01-31 16:47:16 +0100

    add counter feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
 def reverse(request):
     user_text = request.GET['user_text']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'user_text': user_text, 'reversedtext': reversed_text})
+    count_text = len(user_text.split())
+    return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text, 'count_text': count_text})
